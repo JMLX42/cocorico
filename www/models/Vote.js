f5787cc04aff163b963d03ad0dc4a106faa9d692
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-10-26 09:00:13 +0100

    Added redis server install and cache the result of Vote.getByPollIdAndVoter.

--- www/models/Vote.js
+++ www/models/Vote.js
@@ -1,6 +1,8 @@
 var keystone = require('keystone');
 var transform = require('model-transform');
 var bcrypt = require('bcrypt');
+var redis = require('redis');
+
 var Types = keystone.Field.Types;
 
 var Vote = new keystone.List('Vote', {
@@ -14,22 +16,59 @@ Vote.add({
 	poll: { type: Types.Relationship, ref: 'Poll', required: true, initial: true }
 });
 
+Vote.schema.post('remove', function(next)
+{
+	var client = redis.createClient();
+	var key = 'vote/' + this.poll + '/' + this.voter;
+
+	client.on('connect', function()
+	{
+		client.del(key, function(err, reply)
+		{
+			next();
+		});
+	});
+});
+
 Vote.getByPollIdAndVoter = function(pollId, voter, callback)
 {
-	Vote.model.find()
-		.where('poll', pollId)
-		.exec(function(err, votes)
+	var client = redis.createClient();
+	var key = 'vote/' + pollId + '/' + voter;
+
+	client.on('connect', function()
+	{
+		client.get(key, function(err, reply)
 		{
-			if (err)
-				return callback(err, null);
+			if (!err && reply)
+				return callback(null, JSON.parse(reply));
 
-			if (votes && votes.length != 0)
-				for (var vote of votes)
-					if (bcrypt.compareSync(voter, vote.voter))
-						return callback(null, vote);
+			Vote.model.find()
+				.where('poll', pollId)
+				.exec(function(err, votes)
+				{
+					if (err)
+						return callback(err, null);
 
-			return callback(null, null);
+					var found = false;
+					if (votes && votes.length != 0)
+						for (var vote of votes)
+							if (bcrypt.compareSync(voter, vote.voter))
+							{
+								client.set(key, JSON.stringify(vote.toJSON()), function(err, reply)
+								{
+									callback(err, err ? null : vote);
+								});
+								found = true;
+								break;
+							}
+
+					if (!found)
+						callback(null, null);
+				});
 		});
+
+	});
+
 }
 
 transform.toJSON(Vote);
