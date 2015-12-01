8b886b139370da8b443544e66bc6e91dd7c31416
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-12-01 09:34:20 +0100

    Update the one and only view to feature the CSRF token as a meta tag.

--- www/routes/views/index.js
+++ www/routes/views/index.js
@@ -4,5 +4,11 @@ exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 
-	view.render('index', { });
+	view.render(
+		'index',
+		{
+			csrfToken: keystone.security.csrf.getToken(req, res),
+			csrfKey: keystone.security.csrf.TOKEN_KEY
+		}
+	);
 }
