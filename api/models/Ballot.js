69119a0fa22dce0f4985f0a93d5859254b4924ba
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2016-04-03 23:54:22 +0200

    Add additional Ballot.status options.

--- api/models/Ballot.js
+++ api/models/Ballot.js
@@ -12,7 +12,12 @@ var Ballot = new keystone.List('Ballot', {
 Ballot.add({
 	value: { type: String, default: true, required: true, initial: true },
 	time: { type: Types.Datetime, default: Date.now },
-	status: { type: Types.Select, options: ['signing', 'pending', 'complete', 'error'], required: true, initial: true},
+	status: {
+		type: Types.Select,
+		options: ['signing', 'pending', 'initialized', 'registered', 'complete', 'error'],
+		required: true,
+		initial: true
+	},
 	voter: { type: String, required: true, initial: true },
 	bill: { type: Types.Relationship, ref: 'Bill', required: true, initial: true },
 	voterAge: { type: Types.Number, required: true, initial: true },
