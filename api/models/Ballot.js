f4f98b24fbb6d2a2f43414b2597eb339a2940b6b
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-09-07 14:04:18 +0200

    Mark all the fields of the Ballot model as read-only.

--- api/models/Ballot.js
+++ api/models/Ballot.js
@@ -4,22 +4,23 @@ var transform = require('model-transform');
 var Types = keystone.Field.Types;
 
 var Ballot = new keystone.List('Ballot', {
-	defaultSort: '-time',
+	defaultSort: '-updatedAt',
+	track: { createdAt: true, updatedAt: true },
 	nodelete: true,
 	nocreate: true
 });
 
 Ballot.add({
-	time: { type: Types.Datetime, default: Date.now },
 	status: {
 		type: Types.Select,
 		options: ['signing', 'queued', 'pending', 'initialized', 'registered', 'complete', 'error'],
 		required: true,
-		initial: true
+		initial: true,
+		noedit: true
 	},
-	voter: { type: String, required: true, initial: true },
-	vote: { type: Types.Relationship, ref: 'Vote', required: true, initial: true },
-	error: { type: String }
+	voter: { type: String, required: true, initial: true, noedit: true  },
+	vote: { type: Types.Relationship, ref: 'Vote', required: true, initial: true, noedit: true  },
+	error: { type: String, noedit: true }
 });
 
 transform.toJSON(Ballot);
