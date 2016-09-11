3e397d05d3aed9525017350e9491406b2985c78e
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-09-11 19:19:58 +0200

    Add a new reusable component to display authentication errors.

---
+++ app/src/script/component/AuthenticationError.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+
+var PropTypes = React.PropTypes;
+
+var Hint = require('./Hint');
+
+var Grid = ReactBootstrap.Grid,
+    Row = ReactBootstrap.Row,
+    Col = ReactBootstrap.Col;
+
+var AuthenticationError = React.createClass({
+
+    render: function() {
+        return (
+            <Grid>
+                <Hint style="danger">
+                    <h3>Ohoo... :(</h3>
+                    <p>Impossible de vous authentifier.</p>
+                </Hint>
+            </Grid>
+        );
+    }
+
+});
+
+module.exports = AuthenticationError;
