494a203e946b8679b218d30114d4082d73d283bb
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-09-18 15:50:54 +0200

    Refactor the handling of disabled state of the ballot value confirm button in VoteRadioButtons.render().

--- app/src/script/component/VoteRadioButtons.jsx
+++ app/src/script/component/VoteRadioButtons.jsx
@@ -29,27 +29,24 @@ var VoteRadioButtons = React.createClass({
     render: function() {
         var labels = this.props.vote.labels;
 
-        var button_opts = {};
-        if (this.state.ballotValue == null) {
-            button_opts['disabled'] = 'disabled';
-        }
-
         return (
             <form>
                 <ul className="list-unstyled" >
-                    {labels.map((label, index) => { return (
-                        <li>
-                            <label className="vote-label">
-                                <input type="radio" name="ballot" value={index}
-                                    onChange={(e) => this.setState({ballotValue: index})}/>
-                                <i>
-                                    <Title text={label}/>
-                                </i>
-                            </label>
-                        </li>
-                    ); })}
+                    {labels.map((label, index) => {
+                        return (
+                            <li>
+                                <label className="vote-label">
+                                    <input type="radio" name="ballot" value={index}
+                                        onChange={(e) => this.setState({ballotValue: index})}/>
+                                    <i>
+                                        <Title text={label}/>
+                                    </i>
+                                </label>
+                            </li>
+                        )
+                    })}
                 </ul>
-                <Button {...button_opts}
+                <Button disabled={!this.state.ballotValue}
                     className="btn-vote btn-primary"
                     onClick={(e) => this.props.onVote(e, this.state.ballotValue)}>
                         {this.getIntlMessage('vote.VALIDATE_BALLOT')}
