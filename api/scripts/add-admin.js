f766408f49c6304d37bd591691c88607e49c9c63
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-09-24 20:07:00 +0200

    Fix add-admin.js script to use /vagrant/api/src as the cwd and properly test and fix /api/scripts linting.

--- api/scripts/add-admin.js
+++ api/scripts/add-admin.js
@@ -1,38 +1,30 @@
-var config = require('../config.json');
 var keystone = require('keystone');
 var Admin = keystone.list('Admin');
 
-module.exports = function(userEmail, userPassword, userIsAdmin, userName, done)
-{
-    Admin.model.find()
-        .where('email', userEmail)
-        .exec(function(err, users)
-        {
-            if (users.length == 0)
-            {
-                var newAdmin = new Admin.model({
-                	email: userEmail,
-                    name: userName,
-                	password: userPassword,
-                	isAdmin: userIsAdmin == 'true'
-                });
-
-                newAdmin.save(function(err)
-                {
-                    if (err)
-                    {
-                      console.log(err);
-                      return done(err);
-                    }
-
-                    console.log('Admin created.');
-                    done();
-                });
-            }
-            else
-            {
-                console.log('Admin already exists.');
-                done();
-            }
+module.exports = function(userEmail, userPassword, userIsAdmin, userName, done) {
+  Admin.model.find()
+    .where('email', userEmail)
+    .exec((err, users) => {
+      if (users.length === 0) {
+        var newAdmin = new Admin.model({
+          email: userEmail,
+          name: userName,
+          password: userPassword,
+          isAdmin: userIsAdmin === 'true',
         });
+
+        return newAdmin.save((saveErr) => {
+          if (saveErr) {
+            console.log(saveErr);
+            return done(saveErr);
+          }
+
+          console.log('Admin created.');
+          return done();
+        });
+      } else {
+        console.log('Admin already exists.');
+        return done();
+      }
+    });
 };
--- api/scripts/import-pages.js
+++ api/scripts/import-pages.js
@@ -1,104 +1,98 @@
 #!/usr/bin/env headstone
-var config = require('../config.json');
-
 var async = require('async');
 var keystone = require('keystone');
-var stringify = require('json-stable-stringify');
 var fs = require('fs');
 var path = require('path');
 
 var Page = keystone.list('Page'),
-    Media = keystone.list('Media');
+  Media = keystone.list('Media');
 
-function find(startPath,filter)
-{
-    var res = [];
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
+function find(startPath,filter) {
+  var res = [];
+  if (!fs.existsSync(startPath)) {
+    console.log('no dir ', startPath);
+    return null;
+  }
 
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (filename.indexOf(filter)>=0) {
-            res.push(filename);
-        };
+  var files=fs.readdirSync(startPath);
+  for (var i=0;i<files.length;i++) {
+    var filename=path.join(startPath,files[i]);
+    if (filename.indexOf(filter)>=0) {
+      res.push(filename);
     };
+  };
 
-    return res;
+  return res;
 }
 
-module.exports = function(done)
-{
-    var pageFiles = find('./db/pages', '.json');
-    var mediaFiles = find('./db/media', '.json');
+module.exports = function(done) {
+  var pageFiles = find('./db/pages', '.json');
+  var mediaFiles = find('./db/media', '.json');
 
-    async.waterfall(
-        mediaFiles.map((mediaFile) => (callback) => {
-            var mediaData = JSON.parse(fs.readFileSync(mediaFile));
+  async.waterfall(
+    mediaFiles.map((mediaFile) => (callback) => {
+      var mediaData = JSON.parse(fs.readFileSync(mediaFile));
 
-            Media.model.findOne({slug: mediaData.slug})
-                .exec((err, media) => {
-                    if (err)
-                        return callback(err);
+      Media.model.findOne({slug: mediaData.slug})
+        .exec((err, media) => {
+          if (err)
+            return callback(err);
 
-                    if (media) {
-                        var time1 = new Date(media.updatedAt).getTime();
-                        var time2 = new Date(mediaData.updatedAt).getTime();
+          if (media) {
+            var time1 = new Date(media.updatedAt).getTime();
+            var time2 = new Date(mediaData.updatedAt).getTime();
 
-                        if (mediaData.updatedAt && time1 == time2)
-                            return callback(null);
-                    }
+            if (mediaData.updatedAt && time1 === time2)
+              return callback(null);
+          }
 
-                    Media.model.update(
-                        {slug: mediaData.slug},
-                        mediaData,
-                        {upsert: true},
-                        (err) => {
-                            console.log('Updated media "' + mediaData.slug + '"');
-                            return callback(err);
-                        }
-                    );
-                });
+          return Media.model.update(
+            {slug: mediaData.slug},
+            mediaData,
+            {upsert: true},
+            (err) => {
+              console.log('Updated media "' + mediaData.slug + '"');
+              return callback(err);
+            }
+          );
+        });
+    }),
+    (err, result) => {
+      async.waterfall(
+        pageFiles.map((pageFile) => (callback) => {
+          var pageData = JSON.parse(fs.readFileSync(pageFile));
+
+          Page.model.findOne({slug: pageData.slug})
+            .exec((err, page) => {
+              if (err)
+                return callback(err);
+
+              if (page) {
+                var time1 = new Date(page.updatedAt).getTime();
+                var time2 = new Date(pageData.updatedAt).getTime();
+
+                if (pageData.updatedAt && time1 === time2)
+                  return callback(null);
+              }
+
+              if (pageData.contentType === 'HTML')
+                pageData.html = fs.readFileSync('./db/pages/' + pageData.slug + '.html', 'utf8');
+              else
+                pageData.markdown = { md : fs.readFileSync('./db/pages/' + pageData.slug + '.md', 'utf8')};
+
+              return Page.model.update(
+                {slug: pageData.slug},
+                pageData,
+                {upsert: true},
+                (err) => {
+                  console.log('Updated page "' + pageData.slug + '"');
+                  return callback(err);
+                }
+              );
+            });
         }),
-        (err, result) => {
-            async.waterfall(
-                pageFiles.map((pageFile) => (callback) => {
-                    var pageData = JSON.parse(fs.readFileSync(pageFile));
-
-                    Page.model.findOne({slug: pageData.slug})
-                        .exec((err, page) => {
-                            if (err)
-                                return callback(err);
-
-                            if (page) {
-                                var time1 = new Date(page.updatedAt).getTime();
-                                var time2 = new Date(pageData.updatedAt).getTime();
-
-                                if (pageData.updatedAt && time1 == time2)
-                                return callback(null);
-                            }
-
-                            if (pageData.contentType == 'HTML')
-                                pageData.html = fs.readFileSync('./db/pages/' + pageData.slug + '.html', 'utf8');
-                            else
-                                pageData.markdown = { md : fs.readFileSync('./db/pages/' + pageData.slug + '.md', 'utf8')};
-
-                            Page.model.update(
-                                {slug: pageData.slug},
-                                pageData,
-                                {upsert: true},
-                                (err) => {
-                                    console.log('Updated page "' + pageData.slug + '"');
-                                    return callback(err);
-                                }
-                            );
-                        });
-                }),
-                done
-            );
-        }
-    );
+        done
+      );
+    }
+  );
 }
