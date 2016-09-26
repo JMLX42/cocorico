a990d4d6f878d92391be670b5ea0d79fc8dd2d31
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-09-26 09:34:53 +0200

    Minor CSS fixes for the page background color and the Error404 margins.

--- app/src/script/page/Error404.jsx
+++ app/src/script/page/Error404.jsx
@@ -1,10 +1,5 @@
 var React = require('react');
-var ReactBootstrap = require('react-bootstrap');
 var ReactIntl = require('react-intl');
-
-var Grid = ReactBootstrap.Grid,
-  Row = ReactBootstrap.Row,
-  Col = ReactBootstrap.Col;
 
 var Error404 = React.createClass({
 
@@ -12,18 +7,12 @@ var Error404 = React.createClass({
 
   render: function() {
     return (
-            <div className="error-404">
-                <Grid>
-                    <Row>
-                        <Col xs={12}>
-                            <div className="error-message">
-                                {this.getIntlMessage('error.ERROR_404')}
-                            </div>
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
-        );
+      <div className="error-404">
+        <div className="error-message">
+          {this.getIntlMessage('error.ERROR_404')}
+        </div>
+      </div>
+    );
   },
 });
 
