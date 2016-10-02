b64b2b3e5a67e9a59cdb1df291532dbb0002617b
Yoann Aubineau <y.aubineau@criteo.com>
2016-10-02 22:04:39 +0200

    Implement basic webhook worker

--- webhook/index.js
+++ webhook/index.js
@@ -1,71 +1,90 @@
 var bunyan = require('bunyan');
 var request = require('superagent');
-var noCache = require('superagent-no-cache')
 
-var log = bunyan.createLogger({name: "webhook"});
+var log = bunyan.createLogger({name: 'webhook'});
 
-var RETRY_DELAY = 60;
+var RETRY_DELAY = 60 * 1000;  // 1 minute in milliseconds
+var RETRY_TTL = 24 * 60 * 60 * 1000;  // 1 day in milliseconds
 
-function assertMessageIsWebhookEvent(msg) {
-    return !!msg.event && !!msg.url && !!msg.createdAt;
+function isWebhookEvent(content) {
+  return !!content.event && !!content.url && !!content.createdAt;
 }
 
-require('amqplib/callback_api').connect(
-    'amqp://localhost',
-    (err, conn) => {
-        if (err != null) {
-            return log.error({error: err}, 'queue connection error');
-        }
+require('amqplib/callback_api').connect('amqp://localhost', (err, conn) => {
+  if (err) {
+    log.error({error: err}, 'Failed to connect to message broker');
+    return;
+  }
 
-        conn.createChannel((err, ch) => {
-            if (err != null) {
-                return log.error({error: err}, 'queue channel creation error');
-            }
-
-            ch.assertQueue('webhooks');
-            ch.consume('webhooks', (msg) => {
-                if (msg !== null) {
-                    msg = JSON.parse(msg.content.toString());
-
-                    if (!assertMessageIsWebhookEvent(msg)) {
-                        // FIXME: major error here, should we stop the service ?
-                        log.error(
-                            { message: msg },
-                            'assertion failed: queue message is not a valid webhook'
-                        );
-                        return ;
-                    }
-
-                    log.info({ webhook: msg }, 'handling webhook');
-
-                    if (!!msg.lastTriedAt && (msg.lastTriedAt - msg.lastTriedAt > RETRY_DELAY)) {
-                        log.info({ webhook: msg }, 'delaying webhook');
-                        ch.nack(msg, false, true);
-                        return;
-                    }
-
-                    log.info({ webhook: msg }, 'calling webhook');
-
-                    request
-                        .user(noCache)
-                        .post(msg.url)
-                        .send({event: msg.event})
-                        .end((err, res) => {
-                            if (err) {
-                                return log.error({ error: err }, 'webhook error');
-                            }
-
-                            if (res.ok) {
-                                log.info({ status: res.status }, 'webhook call succeeded');
-                                ch.ack(msg);
-                            } else {
-                                log.info({ status: res.status }, 'webhook call failed');
-                                msg.lastTriedAt = Date.now() / 1000;
-                                ch.nack(msg, false, true);
-                            }
-                        });
-                }
-            });
-        });
+  conn.createChannel((err2, ch) => {
+    if (err2) {
+      log.error({error: err}, 'Failed to create message channel');
+      return;
     }
-);
+
+    ch.assertQueue('webhooks');
+    ch.consume('webhooks', (msg) => {
+      msg.content = JSON.parse(msg.content.toString());
+
+      // Delay messages already processed recently
+      if (!!msg.content.lastTriedAt) {
+        var elapsedTime = Date.now() - msg.content.lastTriedAt;
+        if (elapsedTime < RETRY_DELAY) {
+          ch.nack(msg);  // append message back to queue
+          return;
+        }
+      }
+
+      // Skip messages failing for too long
+      if (!!msg.content.firstTriedAt) {
+        var elapsedTime = Date.now() - msg.content.firstTriedAt;
+        if (elapsedTime > RETRY_TTL) {
+          log.error({ message: msg },
+            'Message failed for too long, skipping');
+          ch.ack(msg);
+          return;
+        }
+      }
+
+      // Skip invalid messages
+      if (!isWebhookEvent(msg.content)) {
+        log.error({ message: msg }, 'Invalid message, skipping');
+        ch.ack(msg);
+        return;
+      }
+
+      // Send HTTP request to remote webhook
+      log.info({ message: msg }, 'Calling webhook');
+      request
+        .post(msg.content.url)
+        .send({event: msg.content.event})
+        .end((err3, res) => {
+          if (err3 || res.error) {
+            if (err3) {
+              log.error({ error: err3 },
+                'Failed to call webhook, retrying later');
+            } else if (res.error) {
+              log.error({ http_status: res.status },
+                'Failed to call webhook, retrying later');
+            }
+            if (!msg.content.firstTriedAt) {
+              msg.content.firstTriedAt = Date.now();
+            }
+            msg.content.lastTriedAt = Date.now();
+            // Requeue updated message
+            ch.sendToQueue('webhooks',
+              new Buffer(JSON.stringify(msg.content)),
+              { persistent : true }
+            )
+            ch.ack(msg);
+            return;
+          }
+
+          log.info({ http_status: res.status },
+            'Webhook call succeeded');
+          ch.ack(msg);
+        })
+      ;
+    });
+  });
+});
