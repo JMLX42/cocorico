3497c9741b2c9ba962ce6654027f537dcec9cb55
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-10-08 14:02:54 +0200

    Fix run-tests.sh to have return code != 0 when an error occurred.

--- script/run-tests.sh
+++ script/run-tests.sh
@@ -5,19 +5,26 @@ set +e
 
 pushd api
 npm install && npm run build && npm test
-popd
-
-if [[ $rc != 0 ]]; then
+$API_RC=$?
+if [[ $API_RC != 0 ]]; then
     cat /opt/cocorico/log/api-web.forever.log
     cat /opt/cocorico/log/blockchain.log
     cat /opt/cocorico/log/blockchain-worker-vote.forever.log
     cat /opt/cocorico/log/blockchain-worker-ballot.forever.log
 fi
+popd
+
 
 pushd app
 npm install && npm run build && npm test
+$APP_RC=$?
 popd
 
 pushd blockchain-worker
 npm install && npm test
+$BLOCKCHAIN_WORKER_RC=$?
 popd
+
+if [[ $API_RC != 0 ]] || [[ $APP_RC != 0 ]] || [[ $BLOCKCHAIN_WORKER_RC != 0 ]]; then
+    exit 1
+fi
