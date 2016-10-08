2e7e43067e2c8158dc4162b57a184df534bd3ca5
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-10-08 12:52:44 +0200

    Update run-tests.sh to show the relevant logs when the API tests fail.

--- script/run-tests.sh
+++ script/run-tests.sh
@@ -6,6 +6,13 @@ pushd api
 npm install && npm run build && npm test
 popd
 
+if [[ $rc != 0 ]]; then
+    cat /opt/cocorico/log/api-web.forever.log
+    cat /opt/cocorico/log/blockchain.log
+    cat /opt/cocorico/log/blockchain-worker-vote.forever.log
+    cat /opt/cocorico/log/blockchain-worker-ballot.forever.log
+fi
+
 pushd app
 npm install && npm run build && npm test
 popd
