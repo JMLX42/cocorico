7b321155272f77965c7fc71ef42a3f1d089e994e
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-10-25 09:30:10 +0200

    Save the explicitely set locale in a cookie. This way, it can be read and the locale is kept consistant when the user reloads the page after navigating to an URL that lost the "lang" parameter.

--- app/src/script/intl/locale.js
+++ app/src/script/intl/locale.js
@@ -1,4 +1,5 @@
 var deepmerge = require('deepmerge');
+var cookie = require('js-cookie');
 
 var messages = require('/opt/cocorico/app-web/messages.js');
 var config = require('/opt/cocorico/app-web/config.json');
@@ -26,10 +27,20 @@ module.exports = {
       .map(e => e[0] === 'lang' ? e[1] : false)
       .filter(e => !!e)[0];
 
-    if (!locale) {
-      locale = navigator.language.split('-');
-      if (!!locale[1]) {
-        locale = `${locale[0]}-${locale[1].toUpperCase()}`
+    // if the locale was explicitely set using ?lang=
+    if (!!locale) {
+      // we save it for later
+      cookie.set('lang', locale);
+    } else {
+      // we try to read the saved one
+      locale = cookie.get('lang');
+
+      if (!locale) {
+        // finally, we read it from the navigator object
+        locale = navigator.language.split('-');
+        if (!!locale[1]) {
+          locale = `${locale[0]}-${locale[1].toUpperCase()}`
+        }
       }
     }
 
