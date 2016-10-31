27af9cf67d48ca78f43c1aa5075941b71e7b3313
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-10-31 22:05:15 +0100

    Add the updateVote API test module.

---
+++ api/test/updateVote.js
@@ -0,0 +1,13 @@
+import request from './getRequest';
+import getAccessToken from './getAccessToken';
+import getAPIURL from './getAPIURL';
+
+export default async function(vote, data) {
+  const accesToken = await getAccessToken();
+
+  return request
+    .put(getAPIURL('/vote/') + vote.id)
+    .set('Authorization', 'Bearer ' + accesToken)
+    .send(data)
+    .then((res) => res.body.vote);
+}
