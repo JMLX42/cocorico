4c7706a28af1ac59bad2ea9c7049a50798073569
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-11-07 11:50:41 +0100

    Add a redis-based "cache" module for the API.

---
+++ api/src/cache.js
@@ -0,0 +1,14 @@
+import redis from 'redis';
+import promise from 'thenify';
+
+const client = redis.createClient();
+
+export async function get(key) {
+  var value = await promise((...c)=>client.get(...c))(key);
+
+  return !!value ? JSON.parse(value) : null;
+}
+
+export function set(key, value) {
+  client.set(key, JSON.stringify(value));
+}
