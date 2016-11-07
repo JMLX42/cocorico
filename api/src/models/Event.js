846e6f03edc927b173254d7a9406ca679d23ddf3
Jean-Marc Le Roux <jeanmarc.leroux@gmail.com>
2016-11-07 11:41:43 +0100

    Add notive Events to track IP whitelisting/blacklisting.

--- api/src/models/Event.js
+++ api/src/models/Event.js
@@ -10,7 +10,7 @@ const IPAddress = keystone.list('IPAddress');
 var Event = new keystone.List('Event', {
   defaultColumns: 'createdAt, ip, type, message',
   defaultSort: '-createdAt',
-  track: { createdAt: true, updatedAt: true },
+  track: { createdAt: true, createdBy: true },
   nodelete: config.env !== 'development',
   nocreate: true,
   noedit: true,
@@ -21,9 +21,9 @@ Event.add({
   type: {
     type: Types.Select,
     options: [
-      'emerg',
+      'emergency',
       'alert',
-      'crit',
+      'critical',
       'error',
       'warning',
       'notice',
@@ -41,14 +41,47 @@ IPAddress.schema.post('remove', async function(removed) {
   await Event.model.find({ip:removed}).remove().exec();
 });
 
+IPAddress.schema.post('save', async function() {
+  if (this.wasNew)
+    return;
+
+  if (this.blacklistedWasModified) {
+    if (this.blacklisted && this.numWarningsLeft > 0) {
+      await Event.model({ip: this, type: 'notice', message: 'IP blacklisted'}).save();
+    }
+    if (!this.blacklisted) {
+      await Event.model({ip: this, type: 'notice', message: 'IP removed from blacklist'}).save();
+    }
+  }
+
+  if (this.whitelistedWasModified) {
+    if (this.whitelisted) {
+      await Event.model({ip: this, type: 'notice', message: 'IP whitelisted'}).save();
+    } else {
+      await Event.model({ip: this, type: 'notice', message: 'IP removed from whitelist'}).save();
+    }
+  }
+});
+
 Event.logWarningEventAndBlacklist = async function(requestOrIP, message) {
   const address = await IPAddress.getIPAddress(requestOrIP);
+
+  // The client might have pushed multiple requests at the same time.
+  // One of the earlier requests might have set the IP as blacklisted but the
+  // later requests were already through before iptables was updated.
+  // Thus, we can get "ghosts" requests from a blacklisted IP.
+  // Here, we chose to simply ignore them to avoid making the event logs look
+  // like the previous blacklist attempt did not work.
+  if (address.blacklisted) {
+    await Event.model({ip: address, type: 'notice', message: 'extra warning on an already blacklisted IP'}).save();
+    return true;
+  }
 
   await Event.model({ip: address, type: 'warning', message: message}).save();
 
   address.numWarningsLeft -= 1;
 
-  if (address.numWarningsLeft === 0) {
+  if (address.numWarningsLeft <= 0) {
     await Event.model({ip: address, type: 'alert', message: 'no warning left, blacklisting IP'}).save();
     await address.blacklist();
 
